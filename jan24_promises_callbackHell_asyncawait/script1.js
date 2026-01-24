c137311c8efd8a144c06df0697712b2261d1db42
yashmmith <samindlayashmmithsaii3@gmail.com>
2026-01-25 00:08:28 +0530

    added common js functions in script1.js file

---
+++ jan24_promises_callbackHell_asyncawait/script1.js
@@ -0,0 +1,78 @@
+const str="singh";
+1//const stringlength=str.length;
+2//str.indexOf(target);
+3//str.lastIndexof(target);
+4//str.sclice(start,unilend);
+5//str.subdtr(start,noofchars);
+6//str.replace(target,newreplacemnet);
+7//str.split("/");
+8//str.trim();
+9//str.toUppercase();/toLowercase();
+
+
+let a=parseInt("2.14");    op:- 2
+let a=parseFloat("2");    op:- 2.00
+
+let arr=[1,2,3];
+let sarr=[5,6,7];
+arr.push(4);
+let x=arr.pop();
+arr.shift();
+arr.unshift(0);
+arr.concat(sarr);
+arr.forEach((chars)=>{
+    console.log(chars);
+});
+(parameter)=>{
+    this is also a function with one arguement
+}
+
+class name{
+    constructor(a,b,c){
+        this.a=a;
+        this.b=b;
+        this.c=c;
+    }
+    function(){
+        dosomething;
+    }
+}
+
+const today=new Date();
+console.log(today.getFullyear());
+console.log(today.getmonth());
+console.log(today.getDay());
+console.log(today.getHpurs());
+console.log(today.getMinutes());
+console.log(today.getSeconds());
+console.log(today.getTime());  // Time from 1970 jan 1 epoch timestamp
+today.setFullyear(2029);
+today.setMonth(04);
+
+
+const user={
+    name:"yash"
+};
+let x=JSON.stringify(user);
+let y=JSON.parse(x);
+
+Math.round(value);
+Math.ceil(value);
+Math.floor(value);
+Math.random(value);
+Math.max(value/arr);
+Math.min(value/arr);
+Math.pow(value,power);
+
+
+
+let a={
+    name:"yash
+};
+a.keys();
+a.values();
+a.entries();
+a.hasOwnproperty(property);
+
+
+
